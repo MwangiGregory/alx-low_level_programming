484bf2d0e8bdafc623393e1c6c3bb3308952322c
greg <gmwaburi@gmail.com>
2022-03-10 16:54:45 -0800

    Apply betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ *main - Entry Point
+ *@message - array
+ *
+ * Description: prints a string
+ * Return: 0 (Successful)
+ */
 int main(void)
 {
 	char message[60] = "\"Programming is like building a multilingual puzzle";
