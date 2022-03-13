623b2b14bd1d54e298aedd9bf284b359eaa2ca2e
greg <gmwaburi@gmail.com>
2022-03-13 09:58:37 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *@character - stores a character in the alphabet
+ *
+ * Description - prints alphabet in lowercase
+ * Return: 0 successful 1 failed execution.
+ */
+int main(void)
+{
+	char character = 'a';
+
+	while (character <= 'z')
+	{
+		putchar(character);
+		character++;
+	}
+	return (0);
+}
