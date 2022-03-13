419dceffbf9818f59f33196ac17a379ff43fd184
greg <gmwaburi@gmail.com>
2022-03-13 11:43:06 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints numbers 0-9
+ * Return: 0 successful, 1 failed execution
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		printf("%d", num);
+	}
+	putchar('\n');
+	return (0);
+}
