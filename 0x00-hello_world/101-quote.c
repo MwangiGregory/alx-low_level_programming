64ca09bc0d1a81a48ddd542ce9daf9a27c66ccc3
MwangiGregory <gmwaburi@gmail.com>
2022-03-14 10:44:10 +0000

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - entry point
+ *Return: Always 1, failed execution
+ */
+int main(void)
+{
+	fprintf(stderr,"and that piece of art is useful\""
+			" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
