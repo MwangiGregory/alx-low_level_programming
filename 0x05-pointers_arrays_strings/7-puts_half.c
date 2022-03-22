5fbe3d5943081440261ccc266d270283d5451228
MwangiGregory <gmwaburi@gmail.com>
2022-03-22 13:05:06 +0000

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half the string
+ * @str: address of first element in string array
+ */
+void puts_half(char *str)
+{
+	int n = 0;
+	int i = 0;
+	char c = *str;
+
+	while (c)
+	{
+		i++;
+		c = *(str + i);
+	}
+	if (i % 2 == 0)
+	{
+		n = i / 2;
+	}
+	else
+	{
+		n = (i - 1) / 2;
+		n++;
+	}
+	while (n <= i)
+	{
+		c = *(str + n);
+		_putchar(c);
+		n++;
+	}
+	_putchar('\n');
+}
