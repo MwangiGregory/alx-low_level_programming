37637b049968ff08b127a417f15f97ddc7598af6
MwangiGregory <gmwaburi@gmail.com>
2022-04-04 09:10:31 +0000

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - entry point
- * Prints all the arguments it receives including the 
+ * Prints all the arguments it receives including the
  * first one.
  * @argc: size of argv
  * @argv: array of pointers to strings
