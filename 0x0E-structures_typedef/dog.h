92ed0486f740c534de54bfc8b35e431b3fcdbdf2
MwangiGregory <gmwaburi@gmail.com>
2022-04-11 19:35:39 +0000

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - a new type
+ * @name: pointer to name of dog
+ * @age: age of dog
+ * @owner: pointer to owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
