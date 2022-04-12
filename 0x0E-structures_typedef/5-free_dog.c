8f515c446cf72c4480b45470ad3b0ad1e5f0c7fa
MwangiGregory <gmwaburi@gmail.com>
2022-04-12 12:59:39 +0000

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - frees memory previously dogs
+ * @d: pointer to a struct dog
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d);
+}
