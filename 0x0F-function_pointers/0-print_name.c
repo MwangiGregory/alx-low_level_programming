86b3b98af39a2b4ecda794acf0956e1373ae4109
MwangiGregory <gmwaburi@gmail.com>
2022-04-13 10:07:38 +0000

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+/**
+ * print_name - prints a name via callbacks
+ * @name:pointer to a string
+ * @f:pointer to a function that takes a pointer-to-char as an argument
+ * and returns void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL)
+		return (NULL);
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,4 @@
+#ifndef FUNC_PTRS
+#define FUNC_PTRS
+void print_name(char *name, void (*f)(char *));
+#endif
