bc83b77872c23370fa2e70ee2acbf03bd9e2095a
MwangiGregory <gmwaburi@gmail.com>
2022-04-13 15:00:30 +0000

    3-op_functions.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,39 @@
+/**
+ * op_add - sums and b
+ * @a:integer
+ * @b:integer
+ *
+ * Return: Sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_add - finds the difference
+ * @a:integer
+ * @b:integer
+ *
+ * Return: Difference 
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
