41e7a1ba9b7116e03a4ffe381bbd65022ca0fe02
MwangiGregory <gmwaburi@gmail.com>
2022-04-14 21:21:12 +0000

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -29,6 +29,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			printf("%d", va_arg(ap, int));
 		}
 	}
-	printf("%d\n", va_arg(ap, int));
+	printf("%d", va_arg(ap, int));
 	va_end(ap);
+	printf("\n");
 }
