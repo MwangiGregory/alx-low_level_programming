6f8e862cfcafcf185b991882590be123b67131e2
MwangiGregory <gmwaburi@gmail.com>
2022-04-26 19:45:26 +0000

    put function that adds node at index 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -77,39 +77,3 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	*head = temp;
 	return (temp);
 }
-
-/**
- * add_nodeint_end - adds a new node at the end of a listint_t list
- * @head: pointer to a pointer that points to a listint_t list
- * @n: integer
- * Return: address of the new element, or NULL if it failed
- */
-listint_t *add_nodeint_end(listint_t **head, const int n)
-{
-	listint_t *new_node = NULL;
-	listint_t *last_node = *head;
-
-	if (*head == NULL)
-	{
-		new_node = malloc(sizeof(listint_t));
-		if (new_node == NULL)
-			return (NULL);
-		new_node->n = n;
-		new_node->next = NULL;
-		*head = new_node;
-	}
-	else
-	{
-		while (last_node->next)
-		{
-			last_node = last_node->next;
-		}
-		new_node = malloc(sizeof(listint_t));
-		if (new_node == NULL)
-			return (NULL);
-		new_node->n = n;
-		new_node->next = NULL;
-		last_node->next = new_node;
-	}
-	return (new_node);
-}
