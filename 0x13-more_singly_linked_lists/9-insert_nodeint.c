fa067a4dc57b6c64b2d1a0f49aa8e330c7809c1b
MwangiGregory <gmwaburi@gmail.com>
2022-04-26 14:19:08 +0000

    bug fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,6 +1,45 @@
 #include "lists.h"
 
 /**
+ * insert_nodeint_at_index - inserts a new node at a given position
+ * @head: pointer to a pointer that points to a listint_t list
+ * @idx: index of the list where the new node should be added
+ * @n: intger
+ * Return: address of the new node, or NULL if it failed
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+        listint_t *current_node = NULL;
+        listint_t *temp = NULL;
+        unsigned int i = 0;
+        unsigned int len = 0;
+	const int num = n;
+
+        len = listint_len(*head);
+        if (idx == 0)
+                return (add_nodeint(head, num));
+        else if (idx == len)
+                return (add_nodeint_end(head, num));
+        else if (idx > len)
+                return (NULL);
+
+        current_node = *head;
+        while (i < (idx - 1))
+        {
+                current_node = current_node->next;
+                i++;
+        }
+        temp = malloc(sizeof(listint_t));
+        if (temp == NULL)
+                return (NULL);
+        temp->n = n;
+        temp->next = current_node->next;
+        current_node->next = temp;
+
+        return (temp);
+}
+
+/**
  * listint_len - finds the number of elements in a linked listint_t list
  * @h: pointer to the next node
  * Return: number of elements in h.
@@ -21,35 +60,56 @@ size_t listint_len(const listint_t *h)
 }
 
 /**
- * insert_nodeint_at_index - inserts a new node at a given position
+ * add_nodeint - adds a new node at the beginning of a listint_t list
  * @head: pointer to a pointer that points to a listint_t list
- * @idx: index of the list where the new node should be added
- * @n: intger
- * Return: address of the new node, or NULL if it failed
+ * @n: integer
+ * Return: Address of the new element, NULL if it failed
  */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *current_node = NULL;
 	listint_t *temp = NULL;
-	unsigned int i = 0;
-	unsigned int len = 0;
 
-	len = listint_len(*head);
-	if (idx == 0 || idx + 1 > len)
-		return (NULL);
-
-	current_node = *head;
-	while (i < (idx - 1))
-	{
-		current_node = current_node->next;
-		i++;
-	}
 	temp = malloc(sizeof(listint_t));
 	if (temp == NULL)
 		return (NULL);
 	temp->n = n;
-	temp->next = current_node->next;
-	current_node->next = temp;
-
+	temp->next = *head;
+	*head = temp;
 	return (temp);
+}
+
+/**
+ * add_nodeint_end - adds a new node at the end of a listint_t list
+ * @head: pointer to a pointer that points to a listint_t list
+ * @n: integer
+ * Return: address of the new element, or NULL if it failed
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new_node = NULL;
+	listint_t *last_node = *head;
+
+	if (*head == NULL)
+	{
+		new_node = malloc(sizeof(listint_t));
+		if (new_node == NULL)
+			return (NULL);
+		new_node->n = n;
+		new_node->next = NULL;
+		*head = new_node;
+	}
+	else
+	{
+		while (last_node->next)
+		{
+			last_node = last_node->next;
+		}
+		new_node = malloc(sizeof(listint_t));
+		if (new_node == NULL)
+			return (NULL);
+		new_node->n = n;
+		new_node->next = NULL;
+		last_node->next = new_node;
+	}
+	return (new_node);
 }
