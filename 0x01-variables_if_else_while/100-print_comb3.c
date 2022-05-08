b6534d040cdb250186ae858fa38a478beff1cb45
MwangiGregory <gmwaburi@gmail.com>
2022-05-08 07:43:15 +0000

    Shift definition of _putchar to start of file

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <unistd.h>
-#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 /**
  * main - program that prints all possible different combinations
@@ -49,16 +60,4 @@ int main(void)
 		}
 	}
 	return (0);
-}
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
 }
