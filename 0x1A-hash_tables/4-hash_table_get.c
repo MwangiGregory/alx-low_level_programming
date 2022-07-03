39f7791275458474eda08d9d0c11720b0e5697fb
MwangiGregory <gmwaburi@gmail.com>
2022-07-03 19:56:27 +0000

    Check for invalid hash table and key

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -15,6 +15,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index = 0;
 	hash_node_t *current_node = NULL;
 
+	if (!ht || !key || *key == '\0')
+		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	current_node = ht->array[index];
 	while (current_node)
