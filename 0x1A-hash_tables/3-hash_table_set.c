7506c5215685cc42701324debbe7584ccca6d9a7
MwangiGregory <gmwaburi@gmail.com>
2022-07-03 19:07:57 +0000

    Replace value when same key is entered more than once

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,20 +13,43 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = 0;
-	hash_node_t *head = NULL;
+	hash_node_t *new_node = NULL, *current_node = NULL;
 	int FAILED = 0, SUCCESS = 1;
 
 	if (!ht)
 		return (FAILED);
-	index = key_index((const unsigned char *)key, ht->size);
-	head = malloc(1 * sizeof(hash_node_t));
-	if (!head)
-		return (FAILED);
 	if (!key || *key == '\0')
 		return (FAILED);
-	head->key = strdup(key);
-	head->value = strdup(value);
-	head->next = ht->array[index];
-	ht->array[index] = head;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	new_node = malloc(1 * sizeof(hash_node_t));
+	if (!new_node)
+		return (FAILED);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = NULL;
+
+	current_node = ht->array[index];
+	if (!current_node)
+	{
+		ht->array[index] = new_node;
+		return (SUCCESS);
+	}
+	while (current_node)
+	{
+		if (strcmp(current_node->key, key) == 0)
+		{
+			free(current_node->value);
+			current_node->value = strdup(value);
+			free(new_node->key);
+			free(new_node->value);
+			free(new_node);
+			return (SUCCESS);
+		}
+		current_node = current_node->next;
+	}
+	current_node = ht->array[index];
+	new_node->next = current_node;
+	ht->array[index] = new_node;
 	return (SUCCESS);
 }
