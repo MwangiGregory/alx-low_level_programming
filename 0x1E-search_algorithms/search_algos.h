e7b6a0a8e6daee8868fe3c4a82bbcda3cd350e06
MwangiGregory <gmwaburi@gmail.com>
2023-01-14 09:28:54 +0300

    Defered to cohort 8

--- 0x1E-search_algorithms/search_algos.h
+++
@@ -1,6 +0,0 @@
-#ifndef SEARCH_ALGORITHMS
-#define SEARCH_ALGORITHMS
-int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
-
-#endif /*SEARCH_ALGORITHMS*/
